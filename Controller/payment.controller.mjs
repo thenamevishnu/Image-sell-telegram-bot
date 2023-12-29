784721a7cf12e0f38dc43269f9913bbd93b78f52
thenamevishnu <mail.vishnumk@gmail.com>
2023-12-29 08:16:45 +0530

    testing IPN post data

--- Controller/payment.controller.mjs
+++ Controller/payment.controller.mjs
@@ -1,6 +1,8 @@
 import env from "dotenv"
 import crypto from "crypto"
 import Bot from "../Telegram/Config.mjs"
+import { Types } from "mongoose"
+import { cartDB } from "../Models/cart.model.mjs"
 env.config()
 
 const paymentCallback = async (req, res) => {
@@ -14,6 +16,21 @@ const paymentCallback = async (req, res) => {
             if (postData.type === "payment") {
                 const status = postData.status
                 if (status === "Waiting") {
+                    const cart = await cartDB.aggregate([
+                        {
+                            $match: {
+                                _id: new Types.ObjectId(cartId)
+                            }
+                        }, {
+                            $lookup: {
+                                from: "products",
+                                localField: "product_id",
+                                foreignField: "_id",
+                                as: "product"
+                            }
+                        }
+                    ])
+                    console.log(cart);
                     return await Bot.sendMessage(postData.description, `🕛 (<code>#${postData.orderId}</code>) ${cartId} Waiting for payment...`, {
                         parse_mode: "HTML"
                     }) 
@@ -24,9 +41,24 @@ const paymentCallback = async (req, res) => {
                     })
                 }
                 if (status === "Paid") {
-                    return await Bot.sendMessage(postData.description, `✅ <code>#${postData.orderId}</code>) Payment is confirmed`, {
+                    await Bot.sendMessage(postData.description, `✅ <code>#${postData.orderId}</code>) Payment is confirmed`, {
                         parse_mode: "HTML"
                     })
+                    const cart = await cartDB.aggregate([
+                        {
+                            $match: {
+                                _id: new Types.ObjectId(cartId)
+                            }
+                        }, {
+                            $lookup: {
+                                from: "products",
+                                localField: "product_id",
+                                foreignField: "_id",
+                                as: "product"
+                            }
+                        }
+                    ])
+                    console.log(cart);
                 }
             }
         } else {
